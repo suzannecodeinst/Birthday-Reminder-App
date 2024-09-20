92ec7a3623bc716c3cf368a57b3e7b523f89e2e7
Suzanne Deakin <snfa4gse@students.codeinstitute.net>
2024-09-20 09:22:09 +0000

    add first user coice function, and print to terminal the choice made

--- run.py
+++ run.py
@@ -16,7 +16,27 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('birthday_spreadsheet') 
 
 
-jan = SHEET.worksheet('Jan')
+def first_user_choice():
+    """
+    Welcome user and ask them to choose
+    1- check for birthdays
+    2- add a new birthday to the spreadsheet
+    """
 
-data = jan.get_all_values()
-print(data)
+    print("Welcome to your Birthday Reminder App.")
+    print("Please enter")
+    print("1 to check for birthdays, or")
+    print("2 to add a birthday to the spreadsheet.\n")
+
+    choice = input("Enter '1' or '2' to make your selection here:")
+    choice = int(choice)
+    if choice == 1:
+        print("You chose to check for birthdays.")
+    elif choice == 2:
+        print("You chose to add a new birthday")    
+    else:
+        print("You need to type 1 or 2 to make a choice.")    
+
+    
+
+first_user_choice()    

d6bc4912b82954517ceb0b3cfa7e31d6be0290e4
Suzanne Deakin <snfa4gse@students.codeinstitute.net>
2024-09-20 12:08:58 +0000

    start adding functions to choose a month, and validate function to check input is correct

--- run.py
+++ run.py
@@ -33,7 +33,7 @@ def first_user_choice():
         choice = int(choice)
     
         if validate_data(choice):
-            print("works")
+            #print("works")
             break
 
 def validate_data(values):
@@ -51,9 +51,74 @@ def validate_data(values):
         print("You need to type 1 or 2 to make a choice.")
         return False 
         
-                   
-
     print(values)    
+
+def select_month(choice = 1):
+    """
+    Asks the user to choose a month
+    asks for an input of a number from 1 to 12
+    """
+    while True:
+        print("Which month would you like?")
+        print("All months are numbered 1 to 12, so January is 1, February is 2 etc.")
+        b_month = input("Enter a number to make your selection here:")
+        b_month = int(b_month)
+
+        if validate_month_data(b_month):
+            print("you chose month..")
+            break
+
+
+          
+
+def validate_month_data(values):
+    """
+    Checks user choice input for a month is between 1 and 12 inclusive.
+    Raises an error if not correct input.
+    """
+    if int(values) == 1:
+        print("January")
+        return True
+    elif int(values) == 2:
+        print("February") 
+        return True
+    elif int(values) == 3:
+        print("March") 
+        return True  
+    elif int(values) == 4:
+        print("April") 
+        return True 
+    elif int(values) == 5:
+        print("May") 
+        return True
+    elif int(values) == 6:
+        print("June") 
+        return True
+    elif int(values) == 7:
+        print("July") 
+        return True
+    elif int(values) == 8:
+        print("August") 
+        return True
+    elif int(values) == 9:
+        print("September") 
+        return True
+    elif int(values) == 10:
+        print("October") 
+        return True
+    elif int(values) == 11:
+        print("November") 
+        return True 
+    elif int(values) == 12:
+        print("December") 
+        return True                                    
+    else:
+        print("You need to type between 1 and 12 to make choose a month.")
+        return False 
+        
+    print(values)      
+   
        
 
-first_user_choice()    
+first_user_choice() 
+select_month()  
